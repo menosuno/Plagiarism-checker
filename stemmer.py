50501f893289ba39279f553091d0b976989e4347
Thanigaivelan <menosuno.exitcode@gmail.com>
2022-01-02 23:28:58 +0530

    initial commit

--- stemmer.py
+++
@@ -1,9 +0,0 @@
-from nltk.stem import PorterStemmer
-porter = PorterStemmer()
-
-def stemmer(token_words):
-    stem_sentence=[]
-    for word in token_words:
-        stem_sentence.append(porter.stem(word))
-        stem_sentence.append(" ")
-    return "".join(stem_sentence)
